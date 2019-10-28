6f4d328546939514e53bb66989273efc664f9aca
Sijey <Popov040391@gmail.com>
2019-10-28 17:01:19 +0200

    Updates

---
+++ precache-manifest.3e74a34495a1f4602428d00ec856fc22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4fa4084e537aa40814a8b0d0fc668b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60e772a5576326dae7ee",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "44eac7c1540a2ca2655c",
+    "url": "./static/js/2.7d73443f.chunk.js"
+  },
+  {
+    "revision": "60e772a5576326dae7ee",
+    "url": "./static/js/main.a038260b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
